119e4f125622f8ddc40209c76343623d0db1ba06
shiyuan2he <shiyuan4work@sina.com>
2017-12-12 11:39:40 +0800

    spingcloud配置中心实现高可用

---
+++ springcloud-config-client/src/main/java/com/hsy/springcloud/config/client/SpringCloudConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.hsy.springcloud.config.client;
+
+/**
+ * @author heshiyuan
+ * @description <p></p>
+ * @path springboot.demo/com.hsy.springboot.demo.springboot.jdbc
+ * @date ${data} ${time}
+ * @github http://github.com/shiyuan2he
+ * @email shiyuan4work@sina.com
+ * Copyright (c) 2017 shiyuan4work@sina.com All rights reserved.
+ * @price ¥5    微信：hewei1109
+ */
+public class SpringCloudConfigClientApplication {
+}
---
+++ springcloud-config-server/src/main/java/com/hsy/springcloud/server/config/SpringCloudConfigServerApplication.java
@@ -0,0 +1,25 @@
+package com.hsy.springcloud.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author heshiyuan
+ * @description <p></p>
+ * @path springboot.demo/com.hsy.springboot.demo.springboot.jdbc
+ * @date ${data} ${time}
+ * @github http://github.com/shiyuan2he
+ * @email shiyuan4work@sina.com
+ * Copyright (c) 2017 shiyuan4work@sina.com All rights reserved.
+ * @price ¥5    微信：hewei1109
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class SpringCloudConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class,args) ;
+    }
+}

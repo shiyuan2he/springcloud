3bfa1a65211de6cfec3cab34ff575f8dc41ac2c6
shiyuan2he <shiyuan4work@sina.com>
2017-12-24 16:43:49 +0800

    build

--- springcloud-hystirx-dashboard/src/main/java/com/hsy/springcloud/hystirx/dashboard/service/RestfulServiceImpl.java
+++ springcloud-hystirx-dashboard/src/main/java/com/hsy/springcloud/hystirx/dashboard/service/RestfulServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.web.client.RestTemplate;
  * @author heshiyuan
  * @description <p></p>
  * @path springboot.demo/com.hsy.springboot.demo.springboot.jdbc
- * @date ${data} ${time}
  * @github http://github.com/shiyuan2he
  * @email shiyuan4work@sina.com
  * Copyright (c) 2017 shiyuan4work@sina.com All rights reserved.

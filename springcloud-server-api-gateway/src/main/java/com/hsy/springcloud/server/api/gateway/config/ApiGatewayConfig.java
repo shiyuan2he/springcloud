4728296c5058277dd8cdd601cd07a401ee019624
shiyuan2he <shiyuan4work@sina.com>
2018-01-16 18:01:06 +0800

    ss

--- springcloud-server-api-gateway/src/main/java/com/hsy/springcloud/server/api/gateway/config/ApiGatewayConfig.java
+++ springcloud-server-api-gateway/src/main/java/com/hsy/springcloud/server/api/gateway/config/ApiGatewayConfig.java
@@ -9,19 +9,18 @@ import org.springframework.context.annotation.Configuration;
  * @author heshiyuan
  * @description <p></p>
  * @path springboot.demo/com.hsy.springboot.demo.springboot.jdbc
- * @date ${data} ${time}
  * @github http://github.com/shiyuan2he
  * @email shiyuan4work@sina.com
  * Copyright (c) 2017 shiyuan4work@sina.com All rights reserved.
  * @price ¥5    微信：hewei1109
  */
-@Configuration
+//@Configuration
 public class ApiGatewayConfig {
 
-    @Bean
+    /*@Bean
     public AccessFilter accessFilter(){
         return new AccessFilter();
-    }
+    }*/
 
     /*@Bean
     public ErrorFilter errorFilter(){

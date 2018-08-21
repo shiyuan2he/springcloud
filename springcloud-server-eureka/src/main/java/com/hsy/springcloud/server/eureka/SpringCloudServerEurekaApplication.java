afe70a4e570cff67d8f6b007cbd57dc95d58a0f5
shiyuan2he <shiyuan4work@sina.com>
2018-08-21 22:03:03 +0800

    build

--- springcloud-server-eureka/src/main/java/com/hsy/springcloud/server/eureka/SpringCloudServerEurekaApplication.java
+++ springcloud-server-eureka/src/main/java/com/hsy/springcloud/server/eureka/SpringCloudServerEurekaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * @description <p></p>
+ * @description <p>服务注册中心启动器</p>
  * @author heshiyuan
  * @date 2017/12/11 10:16
  */

1f410bdb9a661f7619674b8273a3011cd94e0828
shiyuan2he <shiyuan4work@126.com>
2018-12-05 18:26:46 +0800

    spring cloud eureka build fix

--- springcloud-server-eureka/src/main/java/com/hsy/springcloud/server/eureka/SpringCloudServerEurekaApplication.java
+++ springcloud-server-eureka/src/main/java/com/hsy/springcloud/server/eureka/SpringCloudServerEurekaApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * @description <p>服务注册中心启动器</p>
+ * @description <p>
+ *     服务注册中心启动器
+ *     注解启动一个服务注册中心提供给其他应用进行会话
+ * </p>
  * @author heshiyuan
  * @date 2017/12/11 10:16
  */
-@EnableEurekaServer // 注解启动一个服务注册中心提供给其他应用进行会话
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringCloudServerEurekaApplication {
     public static void main(String[] args) {
